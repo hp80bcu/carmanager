7bc294bb7a2267ed454e6573efed04fc3b0a333d
suohyoon <dbstndh12@gmail.com>
2024-10-10 21:38:05 +0900

    front-back connect success

--- src/main/java/com/example/carmanager/CarmanagerApplication.java
+++ src/main/java/com/example/carmanager/CarmanagerApplication.java
@@ -2,12 +2,13 @@ package com.example.carmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CarmanagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarmanagerApplication.class, args);
 	}
-
 }
