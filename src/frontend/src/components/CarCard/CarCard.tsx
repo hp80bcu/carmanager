039b89288ade9fb39b3e434c68fa19cca4e864b7
HeoSeongMun <100749666+HeoSeongMun@users.noreply.github.com>
2024-11-14 13:56:29 +0900

    1
    1

--- src/frontend/src/components/CarCard/CarCard.tsx
+++ src/frontend/src/components/CarCard/CarCard.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import './CarCard.css'; // CSS 파일
 
-interface CarProps {
-  id: number;
-  model: string;
-  year: number;
-  mileage: number;
-  price: number;
-  imageUrl: string; // 이미지 URL
+interface CarCardProps {
+  imageUrl: string;
+  name: string;
+  year: string;
+  mileage: string;
+  location: string;
+  fuelType: string;
+  price: string;
 }
 
-const CarCard: React.FC<CarProps> = ({ id, model, year, mileage, price, imageUrl }) => {
+const CarCard: React.FC<CarCardProps> = ({ imageUrl, name, year, mileage, location, fuelType, price }) => {
   return (
     <div className="car-card">
-      <img src={imageUrl} alt={model} />
-      <h2>{model}</h2>
-      <p>연식: {year}</p>
-      <p>주행거리: {mileage}km</p>
-      <p>가격: {price}원</p>
-      <button>상세보기</button>
+      <img src={imageUrl} alt={name} className="car-image" />
+      <h3>{name}</h3>
+      <p>{year}</p>
+      <p>{mileage} | {location}</p>
+      <p>{fuelType}</p>
+      <p>{price} 만원</p>
     </div>
   );
 };
